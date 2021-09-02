6035308c1d961191563a78e2228f666210f7f247
reddel <maxavalley@googlemail.com>
2021-09-02 11:04:29 +0200

    Outsourced ToyModel to a separate python script

--- Scripts/PathFinding.py
+++ Scripts/PathFinding.py
@@ -1,8 +1,6 @@
-from mesa import Model
-from mesa.time import RandomActivation
 from AnimateAgents import *
 from InanimateAgents import *
-from mesa.space import MultiGrid
+from Scripts.ToyModel import *
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 
@@ -22,9 +20,9 @@ def create_toy_space():
     This function creates a multi-grid space and fills it with some inanimate agents.
     """
     model = ToyModel(width=20, height=15)
-    # show_visualization()
+    show_visualization()
 
-    return model
+    return model.grid
 
 
 
@@ -75,77 +73,6 @@ def show_visualization():
     server.port = 8521  # The default
     server.launch()
     server.model
-
-
-class ToyModel(Model):
-    """
-    This is just a toy model to set up the grid and visualize it.
-    """
-
-    def __init__(self, width, height):
-
-        self.grid = MultiGrid(width=width, height=height, torus=False)
-        self.schedule = RandomActivation(self)
-
-        self.fill_grid()
-
-    def step(self):
-        print(f'Doing model step #{self.schedule.time}!')
-        self.schedule.step()
-
-
-    def fill_grid(self):
-
-        """Place outer walls"""
-
-        for i in range(self.grid.width):
-            # lowest row
-            if i != 16 and i != 17:  # space for exit
-                self.grid.place_agent(agent=Wall(0, self), pos=(i, 0))
-            # highest row
-            self.grid.place_agent(agent=Wall(0, self), pos=(i, self.grid.height - 1))
-
-        for i in range(self.grid.height):
-            # left col
-            if i != 4 and i != 5:
-                self.grid.place_agent(agent=Wall(0, self), pos=(0, i))
-            # right col
-            if i != 12 and i != 13:
-                self.grid.place_agent(agent=Wall(0, self), pos=(self.grid.width - 1, i))
-
-        """Place exits"""
-
-        # left exit
-        self.grid.place_agent(agent=Exit(0, self), pos=(0, 4))
-        self.grid.place_agent(agent=Exit(0, self), pos=(0, 5))
-
-        # right exit
-        self.grid.place_agent(agent=Exit(0, self), pos=(self.grid.width - 1, 12))
-        self.grid.place_agent(agent=Exit(0, self), pos=(self.grid.width - 1, 13))
-
-        # lower exit
-        self.grid.place_agent(agent=Exit(0, self), pos=(16, 0))
-        self.grid.place_agent(agent=Exit(0, self), pos=(17, 0))
-
-        """Place inner walls"""
-
-        # horizonal walls
-        for i in range(self.grid.width):
-            if i < 8:
-                self.grid.place_agent(agent=Wall(0, self), pos=(i, 10))
-
-            if i > 8:
-                self.grid.place_agent(agent=Wall(0, self), pos=(i, 3))
-
-        # vertical walls
-        for i in range(self.grid.height):
-            if i < 8:
-                self.grid.place_agent(agent=Wall(0, self), pos=(5, i))
-            if i > 5:
-                self.grid.place_agent(agent=Wall(0, self), pos=(11, i))
-
-        """Place a visitor"""
-        self.grid.place_agent(agent=Visitor(0, self, gender='female'), pos=(1, self.grid.height-2))
 
 
 if __name__ == "__main__":
---
+++ Scripts/ToyModel.py
@@ -0,0 +1,75 @@
+from mesa import Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+from AnimateAgents import *
+from InanimateAgents import *
+
+
+class ToyModel(Model):
+    """
+    This is just a toy model to set up the grid and visualize it.
+    """
+
+    def __init__(self, width, height):
+
+        self.grid = MultiGrid(width=width, height=height, torus=False)
+        self.schedule = RandomActivation(self)
+
+        self.fill_grid()
+
+    def step(self):
+        print(f'Doing model step #{self.schedule.time}!')
+        self.schedule.step()
+
+    def fill_grid(self):
+
+        """Place outer walls"""
+
+        for i in range(self.grid.width):
+            # lowest row
+            if i != 16 and i != 17:  # space for exit
+                self.grid.place_agent(agent=Wall(0, self), pos=(i, 0))
+            # highest row
+            self.grid.place_agent(agent=Wall(0, self), pos=(i, self.grid.height - 1))
+
+        for i in range(self.grid.height):
+            # left col
+            if i != 4 and i != 5:
+                self.grid.place_agent(agent=Wall(0, self), pos=(0, i))
+            # right col
+            if i != 12 and i != 13:
+                self.grid.place_agent(agent=Wall(0, self), pos=(self.grid.width - 1, i))
+
+        """Place exits"""
+
+        # left exit
+        self.grid.place_agent(agent=Exit(0, self), pos=(0, 4))
+        self.grid.place_agent(agent=Exit(0, self), pos=(0, 5))
+
+        # right exit
+        self.grid.place_agent(agent=Exit(0, self), pos=(self.grid.width - 1, 12))
+        self.grid.place_agent(agent=Exit(0, self), pos=(self.grid.width - 1, 13))
+
+        # lower exit
+        self.grid.place_agent(agent=Exit(0, self), pos=(16, 0))
+        self.grid.place_agent(agent=Exit(0, self), pos=(17, 0))
+
+        """Place inner walls"""
+
+        # horizonal walls
+        for i in range(self.grid.width):
+            if i < 8:
+                self.grid.place_agent(agent=Wall(0, self), pos=(i, 10))
+
+            if i > 8:
+                self.grid.place_agent(agent=Wall(0, self), pos=(i, 3))
+
+        # vertical walls
+        for i in range(self.grid.height):
+            if i < 8:
+                self.grid.place_agent(agent=Wall(0, self), pos=(5, i))
+            if i > 5:
+                self.grid.place_agent(agent=Wall(0, self), pos=(11, i))
+
+        """Place a visitor"""
+        self.grid.place_agent(agent=Visitor(0, self, gender='female'), pos=(1, self.grid.height-2))
